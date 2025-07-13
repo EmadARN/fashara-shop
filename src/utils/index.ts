5b6821c4fcad7b350994a538d7085771ae46b5de
emad <arabanemad@gmail.com>
2025-07-13 14:11:14 +0330

    create formatNumberWithDecimal & toSlug in utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,16 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
+
+export const formatNumberWithDecimal = (num: number): string => {
+    const [int, decimal] = num.toString().split('.')
+    return decimal ? `${int}.${decimal.padEnd(2, '0')}` : int
+}
+
+export const toSlug = (text: string): string =>
+    text
+        .toLowerCase()
+        .replace(/[^\w\s-]+/g, '') //کاراکترهای خاص مثل !@#$%^&*() حذف می‌شوند 
+        .replace(/\s+/g, '-') //هر نوع فاصله را به خط فاصله (-) تبدیل می‌کند.
+        .replace(/^-+|-+$/g, '') //هرگونه خط فاصله اضافی در ابتدای یا انتهای رشته را حذف می‌کند.
+        .replace(/-+|/g, '-')
